46228e8b9d5667fd3ece69e00c6c5723dc8c7376
dongguacute <gudupao2022@gmail.com>
2024-11-06 21:37:18 +0800

    新版本

---
+++ .docusaurus/registry.js
@@ -0,0 +1,18 @@
+export default {
+  "0058b4c6": [() => import(/* webpackChunkName: "0058b4c6" */ "@generated/docusaurus-plugin-content-docs/default/p/docs-175.json"), "@generated/docusaurus-plugin-content-docs/default/p/docs-175.json", require.resolveWeak("@generated/docusaurus-plugin-content-docs/default/p/docs-175.json")],
+  "0e384e19": [() => import(/* webpackChunkName: "0e384e19" */ "@site/docs/intro.md"), "@site/docs/intro.md", require.resolveWeak("@site/docs/intro.md")],
+  "17896441": [() => import(/* webpackChunkName: "17896441" */ "@theme/DocItem"), "@theme/DocItem", require.resolveWeak("@theme/DocItem")],
+  "1a4e3797": [() => import(/* webpackChunkName: "1a4e3797" */ "@theme/SearchPage"), "@theme/SearchPage", require.resolveWeak("@theme/SearchPage")],
+  "1f391b9e": [() => import(/* webpackChunkName: "1f391b9e" */ "@theme/MDXPage"), "@theme/MDXPage", require.resolveWeak("@theme/MDXPage")],
+  "393be207": [() => import(/* webpackChunkName: "393be207" */ "@site/src/pages/markdown-page.md"), "@site/src/pages/markdown-page.md", require.resolveWeak("@site/src/pages/markdown-page.md")],
+  "4ba7e5a3": [() => import(/* webpackChunkName: "4ba7e5a3" */ "@site/docs/contribute.md"), "@site/docs/contribute.md", require.resolveWeak("@site/docs/contribute.md")],
+  "5c64c7f1": [() => import(/* webpackChunkName: "5c64c7f1" */ "@site/docs/性別焦慮（GD）/gd.md"), "@site/docs/性別焦慮（GD）/gd.md", require.resolveWeak("@site/docs/性別焦慮（GD）/gd.md")],
+  "5e95c892": [() => import(/* webpackChunkName: "5e95c892" */ "@theme/DocsRoot"), "@theme/DocsRoot", require.resolveWeak("@theme/DocsRoot")],
+  "5e9f5e1a": [() => import(/* webpackChunkName: "5e9f5e1a" */ "@generated/docusaurus.config"), "@generated/docusaurus.config", require.resolveWeak("@generated/docusaurus.config")],
+  "8d63650c": [() => import(/* webpackChunkName: "8d63650c" */ "@site/docs/性別焦慮（GD）/transwomen.md"), "@site/docs/性別焦慮（GD）/transwomen.md", require.resolveWeak("@site/docs/性別焦慮（GD）/transwomen.md")],
+  "a7456010": [() => import(/* webpackChunkName: "a7456010" */ "@generated/docusaurus-plugin-content-pages/default/__plugin.json"), "@generated/docusaurus-plugin-content-pages/default/__plugin.json", require.resolveWeak("@generated/docusaurus-plugin-content-pages/default/__plugin.json")],
+  "a7bd4aaa": [() => import(/* webpackChunkName: "a7bd4aaa" */ "@theme/DocVersionRoot"), "@theme/DocVersionRoot", require.resolveWeak("@theme/DocVersionRoot")],
+  "a94703ab": [() => import(/* webpackChunkName: "a94703ab" */ "@theme/DocRoot"), "@theme/DocRoot", require.resolveWeak("@theme/DocRoot")],
+  "aba21aa0": [() => import(/* webpackChunkName: "aba21aa0" */ "@generated/docusaurus-plugin-content-docs/default/__plugin.json"), "@generated/docusaurus-plugin-content-docs/default/__plugin.json", require.resolveWeak("@generated/docusaurus-plugin-content-docs/default/__plugin.json")],
+  "c141421f": [() => import(/* webpackChunkName: "c141421f" */ "@generated/docusaurus-theme-search-algolia/default/__plugin.json"), "@generated/docusaurus-theme-search-algolia/default/__plugin.json", require.resolveWeak("@generated/docusaurus-theme-search-algolia/default/__plugin.json")],
+  "c4f5d8e4": [() => import(/* webpackChunkName: "c4f5d8e4" */ "@site/src/pages/index.js"), "@site/src/pages/index.js", require.resolveWeak("@site/src/pages/index.js")],};
