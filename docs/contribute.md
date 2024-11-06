1e3c10dbce3d48b609ac5fb6b5c28825919a55d0
dongguacute <gudupao2022@gmail.com>
2024-11-06 21:29:53 +0800

    更新新框架

---
+++ docs/contribute.md
@@ -0,0 +1,33 @@
+# 貢獻者指南
+
+## 簡介
+
+我們支持三種貢獻方式：
+
+1.  [通過 GitHub 發送 PR](/%E7%B0%A1%E4%BB%8B/contribute/#github-pr)
+2.  [通過 GitHub 發送 Issue](/%E7%B0%A1%E4%BB%8B/contribute/#github-Issue)
+3.  [通過 Email 提交](/%E7%B0%A1%E4%BB%8B/contribute/#email)
+
+以下是各種貢獻方式的對比：
+
+| 模式 | 貢獻者顯示 | 簡單 | 審查速度 |
+| - | - | - | - |
+| [PR](/%E7%B0%A1%E4%BB%8B/contribute/#github-pr) | ✓ | ✕ | 快 |
+| [Issue](/%E7%B0%A1%E4%BB%8B/contribute/#github-Issue)  | ✕ | 一般 | 較慢 |
+| [Email](/%E7%B0%A1%E4%BB%8B/contribute/#email) | ✕ | ✓ | 較慢 |
+
+## 通過 GitHub 發送 PR
+
+1. Fork 本倉庫
+2. 進行更改
+3. 提交 PR（請詳細描述更改）
+
+## 通過 GitHub 發送 Issue
+
+1. 編寫内容
+2. 提交 issue（請使用模板）
+
+## 通過 Email 發送
+
+1. 編寫內容
+2. 發送 Email 到 [transsoul@myyahoo.com](mailto:transsoul@myyahoo.com)
---
+++ docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'TransSoul Wiki',
+  tagline: '一個小文檔',
+  favicon: 'img/logo.jpg',
+
+  // Set the production url of your site here
+  url: 'https://transsoul.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'TransSoul', // Usually your GitHub org/user name.
+  projectName: 'Wiki', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-HK',
+    locales: ['zh-HK'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/TransSoul/Wiki/tree/main/',
+        },
+
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    
+    
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'TransSoul',
+        logo: {
+          alt: 'a docs for Transgender',
+          src: 'img/logo.jpg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '文檔',
+          },
+          {
+            href: 'https://github.com/TransSoul/Wiki',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '文檔',
+            items: [
+              {
+                label: '介绍',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: '社交账号',
+            items: [
+              {
+                label: 'X',
+                href: 'https://x.com/TransSoul_Team',
+              },
+            ],
+          },
+          {
+            title: '更多',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} TransSoul Team. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      algolia: {
+      // The application ID provided by Algolia
+      appId: 'NZAZEXXN9H',
+
+      // Public API key: it is safe to commit it
+      apiKey: '4c2d0ccd7ef66b14f9b2b0857959f18f',
+
+      indexName: 'TransSoul_wiki',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
+    }),
+};
+
+export default config;
