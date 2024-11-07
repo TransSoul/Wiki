c2ea723a1a551795d7680e61f3e32d0e7abc7792
Debug967 <1985409711@qq.com>
2024-11-07 21:48:12 +0800

    chore: 清理 Docusaurus 生成的临时文件
    - 删除了多个 Docusaurus 插件的临时配置文件
    - 移除了自动生成的客户端模块和资源文件
    - 清空了 DONT-EDIT-THIS-FOLDER 目录

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/gudupao/Downloads/TransSoul/Wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/gudupao/Downloads/TransSoul/Wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/gudupao/Downloads/TransSoul/Wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/gudupao/Downloads/TransSoul/Wiki/src/css/custom.css"),
-];
